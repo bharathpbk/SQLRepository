f3c7f8d8eafb7508713df4f91886337666ca6c90
bharathpbk <>
2017-10-19 12:48:40 +0530

    This is Initial Table With Repository

---
+++ Tables/dbo.Products.sql
@@ -0,0 +1,18 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Products] (
+		[ProductID]     [int] IDENTITY(1, 1) NOT NULL,
+		[Name]          [nvarchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+		[Quantity]      [int] NULL,
+		[Price]         [float] NULL,
+		CONSTRAINT [PK_Products]
+		PRIMARY KEY
+		CLUSTERED
+		([ProductID])
+	ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Products] SET (LOCK_ESCALATION = TABLE)
+GO
